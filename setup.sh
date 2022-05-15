b7cf03d16df416e83b5ac8f42dbd660960bd3a4f
rjharris01 <richard.harris1999@gmail.com>
2022-05-15 13:15:33 +0100

    added video file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #initial setup
 sudo apt-get update
-sudo apt-get install git
+
+sudo apt-get install mininet -y
+
+sudo apt-get install apache2 -y  
+
+sudo apt-get install x264 -y
+
+sudo apt-get install gpac
+
+sudo apt-get install git 
+
+git clone https://github.com/rjharris01/comp3004.git
+
+
+
 
 mkdir -p test && cd test 
-
-git
